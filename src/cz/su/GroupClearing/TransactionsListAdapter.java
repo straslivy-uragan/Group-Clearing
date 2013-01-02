11b9a6459aa5a3fa4564302c7ff6a3b126fc7844
Strašlivý Uragán <straslivy.uragan@gmail.com>
2013-01-02 23:09:28 +0100

    Comments in TransactionsListAdapter.java

--- src/cz/su/GroupClearing/TransactionsListAdapter.java
+++ src/cz/su/GroupClearing/TransactionsListAdapter.java
@@ -11,34 +11,73 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
 
+/** An adapter providing data for a list of transactions. The layout
+ * of an item is described in <code>transactions_list_item.xml</code>.
+ * An item contains the name, the date, and the amount of the
+ * transaction. Data are retrieved from the database. This adapter is
+ * used by <code>TransactionsListActivity</code> activity.
+ *
+ * @see cz.su.GroupClearing.TransactionsListActivity
+ * @author Strašlivý Uragán
+ * @version 1.0
+ * @since 1.0
+ */
 public class TransactionsListAdapter implements ListAdapter {
+	/** Inflater used for inflating layout of the item.
+	 */
 	LayoutInflater inflater;
+	/** Enclosing context (activity).
+	 */
 	Context context;
+	/** Id of the event of the transactions in the list.
+	 */
 	long myEventId = -1;
+	/** List of observers of data changes in this adapter.
+	 */
 	ArrayList<DataSetObserver> observers;
+	/** Transactions in the list.
+	 */
 	Vector<ClearingTransaction> transactions;
+	/** Database interface object. 
+	 */
 	GCDatabase db = null;
 
+	/** Id of the normal transaction item type.
+	 */
 	public static final int NORMAL_TRANSACTION_TYPE = 0;
 
-	public TransactionsListAdapter(Context aContext, long eventId) {
-		myEventId = eventId;
-		context = aContext;
+	/** Constructs new adapter.
+	 * @param context Enclosing context (activity)
+	 * @param myEventId Id of the activity whose transactions should
+     * be displayed.
+	 */
+	public TransactionsListAdapter(Context context, long myEventId) {
+		this.myEventId = myEventId;
+		this.context = context;
 		inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		observers = new ArrayList<DataSetObserver>();
 		readTransactionsFromDB();
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getCount()
+	 */
 	@Override
 	public int getCount() {
 		return transactions.size();
 	}
 
+	/** Returns the number of transactions in the list.
+	 * @return The number of transactions in the list.
+	 */
 	public int getNumberOfTransactions() {
 		return transactions.size();
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getItem(int)
+	 */
 	@Override
 	public Object getItem(int position) {
 		if (position < transactions.size()) {
@@ -47,6 +86,9 @@ public class TransactionsListAdapter implements ListAdapter {
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getItemId(int)
+	 */
 	@Override
 	public long getItemId(int position) {
 		if (position < transactions.size()) {
@@ -55,11 +97,17 @@ public class TransactionsListAdapter implements ListAdapter {
 		return -1;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getItemViewType(int)
+	 */
 	@Override
 	public int getItemViewType(int position) {
 		return NORMAL_TRANSACTION_TYPE;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getView(int, android.view.View, android.view.ViewGroup)
+	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View rowView = null;
@@ -84,47 +132,75 @@ public class TransactionsListAdapter implements ListAdapter {
 		return rowView;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getViewTypeCount()
+	 */
 	@Override
 	public int getViewTypeCount() {
 		return 1;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#hasStableIds()
+	 */
 	@Override
 	public boolean hasStableIds() {
 		return true;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#isEmpty()
+	 */
 	@Override
 	public boolean isEmpty() {
 		return transactions.isEmpty();
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#registerDataSetObserver(android.database.DataSetObserver)
+	 */
 	@Override
 	public void registerDataSetObserver(DataSetObserver observer) {
 		observers.add(observer);
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#unregisterDataSetObserver(android.database.DataSetObserver)
+	 */
 	@Override
 	public void unregisterDataSetObserver(DataSetObserver observer) {
 		observers.remove(observer);
 	}
 
+	/** Notifies observers about data change.
+	 */
 	public void notifyDataSetChanged() {
 		for (DataSetObserver observer : observers) {
 			observer.onChanged();
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.ListAdapter#areAllItemsEnabled()
+	 */
 	@Override
 	public boolean areAllItemsEnabled() {
 		return true;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.widget.ListAdapter#isEnabled(int)
+	 */
 	@Override
 	public boolean isEnabled(int position) {
 		return true;
 	}
 
+	/** Reads transactions from the database. Transactions are read
+     * using <code>GCDatabase.readTransactionsOfEvent(long)</code>
+     * function. After that observers are notified about data change
+     * by calling <code>notifyDataSetChanged()</code> function.
+	 */
 	public void readTransactionsFromDB() {
 		if (db == null) {
 			db = new GCDatabase(context);
@@ -133,6 +209,11 @@ public class TransactionsListAdapter implements ListAdapter {
 		notifyDataSetChanged();
 	}
 
+	/** Creates new transaction and adds it to the list. It is created
+     * using <code>GCDatabase.createNewTransaction(long)</code>
+     * function.
+	 * @return Newly created transaction.
+	 */
 	public ClearingTransaction createTransaction() {
 		ClearingTransaction aTransaction = null;
 		try {
@@ -145,6 +226,10 @@ public class TransactionsListAdapter implements ListAdapter {
 		return aTransaction;
 	}
 
+	/** Removes transaction at given position from the list and the
+     * database.
+	 * @param position Position of the transaction to be removed.
+	 */
 	public void removeTransactionAtPosition(int position) {
 		if (position < transactions.size()) {
 			db.deleteTransactionWithId(transactions.get(position).getId());
@@ -153,6 +238,8 @@ public class TransactionsListAdapter implements ListAdapter {
 		}
 	}
 
+	/** Closes the database connection.
+	 */
 	public void closeDB() {
 		if (db != null) {
 			db.close();
